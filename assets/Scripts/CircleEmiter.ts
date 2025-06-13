945094d89fc989cbaa40f444d90c15615bf87b0e
AMagicPear <AMagicPear@outlook.com>
2025-06-13 15:00:51 +0800

    对这个谱真的跟有点那个啥大饼一样

--- assets/Scripts/CircleEmiter.ts
+++ assets/Scripts/CircleEmiter.ts
@@ -1,3 +1,4 @@
+import { testScore, ScoreNote, Score } from "./util/scoreTranslator";
 const { ccclass, property } = cc._decorator;
 
 @ccclass
@@ -8,13 +9,60 @@ export default class CircleEmiter extends cc.Component {
     @property(cc.Float)
     rorateSpeed: number = 10
 
-    circles: cc.Node[] = []
+    @property(cc.Label)
+    testHitLabel: cc.Label = null
+
+    @property(cc.AudioSource)
+    audioSource: cc.AudioSource = null
+
+    circles: { noteIndex: number, insAngle: number, node: cc.Node }[] = []
+    drumsToDraw: ScoreNote[] = []
+    incomingIndex: number = 0
+    
+    @property(cc.Float)
+    notesAheadTime: number = 5
+    // elaspedTime: number = 0
+
+    protected onLoad(): void {
+        cc.systemEvent.on(cc.SystemEvent.EventType.KEY_DOWN, this.onKeyDown, this);
+    }
 
     protected start(): void {
+        // this.testHitLabel.enabled = false
+        this.loadScore(testScore)
+        // this.elaspedTime = 0
+        this.incomingIndex = 0
+        console.log(this.drumsToDraw)
+        this.audioSource.play()
     }
 
     protected update(dt: number): void {
-        this.node.angle -= this.rorateSpeed * dt
-        // if(this.node.angle)
+        // this.elaspedTime += dt
+        this.node.angle -= this.rorateSpeed * dt    // 以rorateSpeed转动
+        if (this.incomingIndex != undefined
+            && this.incomingIndex < this.drumsToDraw.length
+            && this.audioSource.getCurrentTime() > this.drumsToDraw[this.incomingIndex].startTime - this.notesAheadTime) {
+            let newCircle = cc.instantiate(this.circleSprite)
+            this.node.addChild(newCircle)
+            newCircle.setRotation(this.node.angle)
+            this.circles.push({
+                noteIndex: this.incomingIndex,
+                insAngle: this.node.angle,
+                node: newCircle
+            })
+            console.log("当前曲谱下标", this.incomingIndex)
+            console.log(this.circles)
+            this.incomingIndex += 1
+        }
+    }
+
+    private loadScore(score: Score) {
+        this.drumsToDraw = score.drums
+        console.log(`已加载曲谱，共${this.drumsToDraw.length}个音符`)
+    }
+
+    private onKeyDown(event: any) {
+        console.log(this.audioSource.getCurrentTime(), event.keyCode)
+        this.testHitLabel.string = event.keyCode
     }
 }
--- assets/Scripts/util/scoreTranslator.ts
+++ assets/Scripts/util/scoreTranslator.ts
@@ -9,28 +9,57 @@ const CircleSpeedFromNoteXi: Record<NoteXi, number> = {
     'H': 1,
 }
 
-interface ScoreNote {
+export interface ScoreNote {
     startTime: number,  // 起始小节号
-    duration: number,   // 持续节拍数
+    // duration: number,   // 持续节拍数
     note: NoteDong,     // 音符
 }
 
-interface Score {
+export interface Score {
     name: string,   // 曲谱名称
     bpm: number,    // 每分钟节拍数
-    drums: ScoreNote[],
+    drums: ScoreNote[], // 音符
 }
 
-let testScore: Score = {
+export let testScore: Score = {
     name: 'testscore',
-    bpm: 90,
+    bpm: 60,
     drums: [{
-        startTime: 0,
-        duration: 1,
+        startTime: 5,
         note: 'E'
     }, {
-        startTime: 1,
-        duration: 1,
+        startTime: 6,
         note: 'E'
-    }]
+    }, {
+        startTime: 8,
+        note: 'E'
+    },{
+        startTime: 9,
+        note: 'E'
+    }, {
+        startTime: 10,
+        note: 'E'
+    }, {
+        startTime: 12,
+        note: 'E'
+    }, {
+        startTime: 13,
+        note: 'E'
+    }, {
+        startTime: 14,
+        note: 'E'
+    }, {
+        startTime: 15,
+        note: 'E'
+    }, {
+        startTime: 16,
+        note: 'E'
+    }, {
+        startTime: 17,
+        note: 'E'
+    }, {
+        startTime: 18,
+        note: 'E'
+    }
+    ]
 }
